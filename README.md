1582bffc7d5967556daa77aa13571576b6ff1839
Luke Vella <luke.vella@sap.com>
2016-11-03 15:29:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Convox Links Issue
+
+This repository demonstrates an issue with convox/rack.
+
+## What to do?
+
+Clone the repository and run `convox doctor` or `convox start`. Either command should crash silently when it tries to run the `client` service.
---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('*', (req, res) => {
+  res.send(process.env.WORKER_URL);
+});
+
+
+app.listen(port, '0.0.0.0', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
